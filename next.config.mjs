4b6fbedd62cf9428f3fd36aa4fb7e14fedc5690a
ginwan <ginwan697@gmail.com>
2024-07-18 18:39:51 +0400

    pwa app done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import withPWA from "next-pwa"; // Import using require
+
+const nextConfig = {
+  reactStrictMode: true,
+  pwa: withPWA({
+    dest: "public", // Destination for generated PWA files
+    register: true, // Enable automatic PWA registration
+    skipWaiting: true, // Allow immediate control over service worker
+    // ... other PWA options (refer to next-pwa documentation)
+  }),
+};
 
 export default nextConfig;
